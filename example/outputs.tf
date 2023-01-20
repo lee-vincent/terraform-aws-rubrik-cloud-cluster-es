8c071157861801f91d66e5fd356391e4a64f2433
lee-vincent <vinnie@vinnielee.io>
2023-01-20 07:46:46 -0500

    set defaults in example for testing

--- example/outputs.tf
+++ example/outputs.tf
@@ -9,6 +9,9 @@ output "workload_instance_private_ip" {
 output "rubrik_ips" {
   value = module.rubrik-cloud-cluster.rubrik_cloud_cluster_ip_addrs[*]
 }
+output "rubrik_ami" {
+  value = module.rubrik-cloud-cluster.rubrik_ami
+}
 output "rubrik_bucket" {
   value = module.rubrik-cloud-cluster.backup_bucket_name
 }
